8522ed565dec426015297ab7716ddaad21a4d872
Ondrej Chaloupka <ochaloup@redhat.com>
2014-02-17 19:00:32 +0100

    Working version of the plugin done

--- README.md
+++ README.md
@@ -1,3 +1,56 @@
-mvn install:install-file -Dfile=target/load-to-system-properties-maven-plugin-1.0-SNAPSHOT.jar -DgroupId=org.jboss.qa -DartifactId=load-to-system-properties-maven-plugin -Dversion=1.0-SNAPSHOT -Dpackaging=jar -DlocalRepositoryPath=$HOME/my-testing/datasources-test/lib
-rm -rf ~/.m2/repository/org/jboss/qa/load-to-system-properties-maven-plugin/
-mvn install:install-file -Dfile=target/load-to-system-properties-maven-plugin-1.0-SNAPSHOT.jar -DgroupId=org.jboss.qa -DartifactId=load-to-system-properties-maven-plugin -Dversion=1.0-SNAPSHOT -Dpackaging=maven-plugin -DlocalRepositoryPath=$HOME/my-testing/datasources-test/lib
+# Maven Properties Loader Plugin #
+
+This is a plugin which aims to load properties from file and let you use it during configuration (bound to phase INITIALIZE by default).
+
+Loaded properties are passed to maven surefire as well. Test class could use them by the System.getProperty() call.
+
+As a bonus a string packing properties to one string is created. It looks in a way -Dname=value. This string property is supposed to be used in arquillian-jboss-container property javaVmArguments.
+
+## How to use it ##
+
+Currently there is no plugin repo with this plugin so you can do it by hacky way like (inspired by http://stackoverflow.com/questions/2065928/maven-2-assembly-with-dependencies-jar-under-scope-system-not-included)
+
+    rm -rf ~/.m2/repository/org/jboss/qa/load-to-system-properties-maven-plugin/
+    mvn clean package
+    mvn install:install-file -Dfile=target/load-to-system-properties-maven-plugin-1.0-SNAPSHOT.jar -DgroupId=org.jboss.qa -DartifactId=load-to-system-properties-maven-plugin -Dversion=1.0-SNAPSHOT -Dpackaging=maven-plugin -DlocalRepositoryPath=<extern_path>
+
+Then in you project you would need to specify local maven repo:
+
+    <pluginRepositories>
+      <pluginRepository>
+        <id>project-internal-lib-repo</id>
+        <url>file://<extern_path></url>
+        <snapshots>
+          <enabled>true</enabled>
+        </snapshots>
+      </pluginRepository>
+    </pluginRepositories>
+
+And then use it like:
+
+     <plugin>
+       <groupId>org.jboss.qa</groupId>
+       <artifactId>load-to-system-properties-maven-plugin</artifactId>
+       <version>1.0-SNAPSHOT</version>
+       <executions>
+         <execution>
+           <configuration>
+             <files>
+               <file>prop.properties</file>
+             </files>
+           </configuration>
+           <goals>
+             <goal>load-properties</goal>
+           </goals>
+         </execution>
+       </executions>
+     </plugin>
+
+And for arquillian.xml you can add something like:
+
+    <container qualifier="JBOSS_AS_MANAGED_7.X">
+      <protocol type="jmx-as7" />
+      <configuration>
+        <property name="javaVmArguments">${loaded.properties:}</property>
+      </configuration>
+    </container>
--- src/main/java/org/jboss/qa/LoadFileToSystemPropertiesMojo.java
+++ src/main/java/org/jboss/qa/LoadFileToSystemPropertiesMojo.java
@@ -5,6 +5,7 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
 
+import org.apache.maven.execution.MavenSession;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugins.annotations.Component;
@@ -14,19 +15,41 @@ import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.project.MavenProject;
 
 /**
+ * The main purpose of this plugin is ability to load properties
+ * from file, being able to handle with them in maven configuration
+ * and pass them to maven surefire plugin. Then to have a simple 
+ * variable string which could be possible to pass to arquillian 
+ * as vm argument (javaVmArguments - used in jboss as container).
+ * 
  * Simple plugin which takes property files and converts them
- * to system properties which could be used by project later on.
+ * to maven project properties which could be used by project later on.
+ * 
+ * It's done in the way that these properties should be passed as system
+ * properties to surefire as well.
+ * 
+ * The Plugin creates a property which would contains all the loaded properties
+ * in the way of -DpropName=propValue
+ * 
+ * Note: check the configuration option <systemPropertiesFile></systemPropertiesFile>
+ * of maven surefire plugin where you can pass system properties from file as well.
  *  
  * @author Ondrej Chaloupka <ochaloup@redhat.com>
  */
-@Mojo(name = "load", defaultPhase = LifecyclePhase.INITIALIZE)
+@Mojo(name = "load-properties", defaultPhase = LifecyclePhase.INITIALIZE)
 public class LoadFileToSystemPropertiesMojo extends AbstractMojo {
-   
+  private static final String ALL_PROPERTIES_NO_SPACE_SUFFIX = ".nospace"; 
+  
   /**
    * Need for being able to fill data to maven project properties.
    */
   @Component
   private MavenProject project;
+  
+  /**
+   * Maven session - used for passing properties to maven surefire.
+   */
+  @Component
+  private MavenSession session;
 
   /**
    * Files that will be search for loading properties.
@@ -37,21 +60,22 @@ public class LoadFileToSystemPropertiesMojo extends AbstractMojo {
   /**
    * Name of system parameter where all properties will be put 
    * in as -DpropName=propValue string.
+   * There will be createad one more property with name ${allPropertiesName}.nospace
+   * which that changes all spaces to underscore character '_'.
    */
   @Parameter(defaultValue = "loaded.properties")
   private String allPropertiesName;
   
-  /**
-   * Saying whether the properties should be pass to project properties as well.
-   * Otherwise just system properties will be filled.
-   */
-  @Parameter
-  private Boolean fillProjectProperty;
 
   public void execute() throws MojoExecutionException {
     getLog().info("Starting loading properties from files");
     
     StringBuffer allPropertiesString = new StringBuffer();
+    // There is a problem of arquillian-container-jboss does parsing the properties
+    // by simple splitting by space - this is a workaround
+    // @see org.jboss.arquillian.container.jbossas.managed_6.JBossASConfiguration#getJavaVmArguments
+    // plus check ALL_PROPERTIES_NO_SPACE_SUFFIX
+    StringBuffer allPropertiesWithoutSpacesString = new StringBuffer();
     
     for(File file: files) {
       if(file.exists()) {
@@ -63,14 +87,22 @@ public class LoadFileToSystemPropertiesMojo extends AbstractMojo {
           fis = new FileInputStream(file);
           loadedProperties.load(fis);
           
-          // System.setProperties(loadedProperties);
           for(String loadedPropName: loadedProperties.stringPropertyNames()) {
-            System.setProperty(loadedPropName, loadedProperties.getProperty(loadedPropName));
-            allPropertiesString.append(" -D" + loadedPropName + "=" + loadedProperties.getProperty(loadedPropName));
+            String loadedPropValue = loadedProperties.getProperty(loadedPropName);
+            getLog().debug("Loading property " + loadedPropName + "=" + loadedPropValue);
             
-            if(fillProjectProperty) {
-              project.getProperties().setProperty(loadedPropName, loadedProperties.getProperty(loadedPropName));
-            }
+            // This seems to be for nothing
+            System.setProperty(loadedPropName, loadedPropValue);
+            // Project would be able to handle with properties
+            project.getProperties().setProperty(loadedPropName, loadedPropValue);
+            // This is a bit hacky way how to force maven surefire to use these properties
+            // see org.apache.maven.plugin.surefire.AbstractSurefireMojo#getUserProperties
+            // method available from maven 2.2
+            session.getUserProperties().setProperty(loadedPropName, loadedPropValue);
+            
+            // Filling property string
+            allPropertiesString.append(" -D" + loadedPropName + "=" + loadedPropValue);
+            allPropertiesWithoutSpacesString.append(" -D" + loadedPropName + "=" + loadedPropValue.replace(" ", "_"));
           }          
           
         } catch (IOException ex) {
@@ -90,10 +122,17 @@ public class LoadFileToSystemPropertiesMojo extends AbstractMojo {
     
     // setting string -Dname=value to system properties
     if(allPropertiesString.length() > 0) {
-      System.setProperty(allPropertiesName, allPropertiesString.toString());
-      if(fillProjectProperty) {
-        project.getProperties().setProperty(allPropertiesName, allPropertiesString.toString());
-      }
+      getLog().info("Setting " + allPropertiesName + "=" + allPropertiesString.toString().trim() + ". There is as well property with name " + 
+          allPropertiesName + ALL_PROPERTIES_NO_SPACE_SUFFIX + " created");
+      
+      // Setting all properties string
+      System.setProperty(allPropertiesName, allPropertiesString.toString().trim());
+      project.getProperties().setProperty(allPropertiesName, allPropertiesString.toString().trim());
+      session.getUserProperties().setProperty(allPropertiesName, allPropertiesString.toString().trim());
+      // All properties string without spaces
+      System.setProperty(allPropertiesName + ALL_PROPERTIES_NO_SPACE_SUFFIX, allPropertiesWithoutSpacesString.toString().trim());
+      project.getProperties().setProperty(allPropertiesName + ALL_PROPERTIES_NO_SPACE_SUFFIX, allPropertiesWithoutSpacesString.toString().trim());
+      session.getUserProperties().setProperty(allPropertiesName + ALL_PROPERTIES_NO_SPACE_SUFFIX, allPropertiesWithoutSpacesString.toString().trim());
     }
     
   }
